2cff8e97e486e46186ec068acf837c3637210d49
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-08-20 18:51:56 +0500

    tailwind conf settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+   container:{
+    center: true,
+    padding: '15px',
+   },
+   colors: {
+    accent: "#FF8F9C",
+    blckish: "#1b1b1b",
+   },
     },
   },
   plugins: [],

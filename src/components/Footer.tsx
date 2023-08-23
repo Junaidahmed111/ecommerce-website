cb3e027ae425ff82972930699909cefdfb37494f
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-08-23 21:59:01 +0500

    fix 4

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-black text-gray-500 text-center py-4 pb-16 md:pb-4">
+    <div className="bg-black text-gray-400 text-center py-4 pb-16 md:pb-4">
       Copyright © Dev Solutions <br /> All Rights Reserved 2023.
     </div>
   );
